3eff3def9f13fef583f076505da2c94660755be9
dhaval312 <shahdhaval660.ds@gmail.com>
2021-02-19 15:41:39 -0700

    Register page

---
+++ RickyTestApp/Controllers/RegisterController.cs
@@ -0,0 +1,81 @@
+﻿using Group8.TravelExperts.Data.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RickyTestApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+
+
+        public ActionResult RegisterDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterDetails(Customer c)
+        {
+            int id = c.CustomerId;
+            var context = new TravelExpertsContext();
+
+            
+
+            
+            context.Customers.Add(c);
+           
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+        // GET: RegisterController/Create
+       
+       
+        //// GET: RegisterController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: RegisterController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: RegisterController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: RegisterController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
