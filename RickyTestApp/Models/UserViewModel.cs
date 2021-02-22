e2c9a2fbf91d107e3d6f69642c7ee59275774969
Stephen Yuzyk <stephen.yuzyk@edu.sait.ca>
2021-02-22 12:30:42 -0700

    Some validation for login

--- RickyTestApp/Models/UserViewModel.cs
+++ RickyTestApp/Models/UserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace RickyTestApp.Models
     //Ricky wrote this code.
     public class UserViewModel
     {
+        [Required(ErrorMessage = "Please enter your username")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
         public string Password { get; set; }
     }
 }
